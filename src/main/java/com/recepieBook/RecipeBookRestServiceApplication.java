32f8fed67eafd98f8778b940297b17ff69545a38
Robin Sjölund <Robin.sjolund@gmail.com>
2016-12-06 15:39:23 +0100

    Initial commit

---
+++ src/main/java/com/recepieBook/RecipeBookRestServiceApplication.java
@@ -0,0 +1,12 @@
+package com.recepieBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeBookRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeBookRestServiceApplication.class, args);
+	}
+}
