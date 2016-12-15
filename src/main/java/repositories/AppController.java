67ede23744182317ca9f506caa86c1bd35051e61
Robin Sjölund <Robin.sjolund@gmail.com>
2016-12-15 17:54:14 +0100

    Fix CORS config, Add Return data to post and delete methods

--- src/main/java/repositories/AppController.java
+++ src/main/java/repositories/AppController.java
@@ -9,6 +9,7 @@ import entities.Category;
 import entities.Recipe;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author Robin_000
  */
-
+@CrossOrigin
 @RestController
 @RequestMapping(value = "/API")
 public class AppController {
@@ -33,36 +34,37 @@ public class AppController {
         this.categoryRepositry = cr;
         this.recipeRepository = rr;
     }
-
+    
     @RequestMapping(value = "/categories", method = RequestMethod.GET)
     public List<Category> getCategories() {
         return categoryRepositry.findAll();
     }
-    
+        
     @RequestMapping(value = "/recipies", method = RequestMethod.GET)
     public List<Recipe> getRecipies() {
         return recipeRepository.findAll();
     }
     
     @RequestMapping(value = "/recipies", method = RequestMethod.POST)
-    public void saveRecipie(@RequestBody Recipe recipe){
+    public List<Recipe> saveRecipie(@RequestBody Recipe recipe){
         recipeRepository.save(recipe);
+        return recipeRepository.findAll();
     }
     
     @RequestMapping(value = "/recipies/{id}", method = RequestMethod.POST)
-    public void deleteRecipie(@PathVariable int id) {
+    public List<Recipe> deleteRecipie(@PathVariable int id) {
         recipeRepository.delete(id);
+        return recipeRepository.findAll();
     }
     
     @RequestMapping(value = "/recipies/{id}", method = RequestMethod.PUT)
-    public void updateRecipie(@PathVariable int id, @RequestBody Recipe newRecipe) {
+    public List<Recipe> updateRecipie(@PathVariable int id, @RequestBody Recipe newRecipe) {
         Recipe oldRecipe = recipeRepository.findOne(id);
         oldRecipe.setCategory(newRecipe.getCategory());
         oldRecipe.setIngredients(newRecipe.getIngredients());
         oldRecipe.setSteps(newRecipe.getSteps());
         oldRecipe.setTitle(newRecipe.getTitle());
         recipeRepository.save(oldRecipe);
-    }
-    
-    
+        return recipeRepository.findAll();
+    }   
 }
